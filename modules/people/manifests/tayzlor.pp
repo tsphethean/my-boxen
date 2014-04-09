ef629fe2af7a80b75152a7a389947390f79750bf
Graham Taylor <graham.x.taylor@capgemini.com>
2014-04-09 16:47:06 +0100

    Update personal manifest

--- modules/people/manifests/tayzlor.pp
+++ modules/people/manifests/tayzlor.pp
@@ -1,6 +1,6 @@
 class people::tayzlor {
   include spotify
-  
+  include zsh 
   include osx::dock::autohide  
   include osx::finder::show_hidden_files
   class { 'osx::global::natural_mouse_scrolling':
