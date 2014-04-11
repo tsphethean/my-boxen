b119296ee47912a1c894ea860b132e31f8f01edf
Tom Phethean <tom.phethean@capgemini.com>
2014-04-11 18:40:21 +0100

    Use ZSH as shell

--- modules/people/manifests/tsphethean.pp
+++ modules/people/manifests/tsphethean.pp
@@ -1,8 +1,24 @@
 class people::tsphethean {
   include flux::beta
   include dropbox
+  include zsh
+  include ohmyzsh
 
   class { 'calibre':
     version => '1.30.0',
   }
+
+  # dotfiles
+  repository { "/Users/tsphethean/src/dotfiles":
+    source => "${::github_login}/dotfiles",
+    require => File["/Users/tsphethean/src"],
+  }
+
+#  exec { "install dotfiles":
+#    provider => shell,
+#    command  => "./script/install",
+#    cwd      => "/Users/tsphethean/src/dotfiles",
+#    creates  => "/Users/${::boxen_user}/.zshrc",
+#    require  => Repository["/Users/tsphethean/src/dotfiles"],
+#  }
 }
